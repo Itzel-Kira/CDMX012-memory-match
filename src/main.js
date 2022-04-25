c6e64d9d3c14053e372d98ce89afce82f8d39ade
Itzel-Kira <97539002+Itzel-Kira@users.noreply.github.com>
2022-04-25 11:00:06 -0500

    Funciones que ya casi funcionan

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import App from "./components/App.js";
 import pokemon from "./data/pokemon/pokemon.js";
-document.getElementById("root").appendChild(App());
+//document.getElementById("root").appendChild(App());
 
 const pokemon1 = pokemon.items;
 //console.log(pokemon1); //es la primera copia de mi arreglo
@@ -24,20 +24,17 @@ let random = "";
 for (let index = 0; index < 18; index++) {
   const print = document.getElementById("pokeCards");
   random += `
-<div class="cardsContainer">
-        <div id="cardP${index}" class="cardP">
+<div class="cardP">
+        <div id="cardsContainer${index}" class="cardsContainer">
         <div class ="face frontCard">
                 <img src="${"../pokemonimg/pokebola.png"}" id="pokebola">
         </div>
 
-
-                <div id="${index}" class="face backCard">
+        <div id="${index}" class="face backCard">
                         <img id="pokeName${index}" src="${
     pokemon3[index].image
   }" alt="${pokemon3[index].id}" />
         </div>
-
-        
 </div>
 </div>
 `;
@@ -45,7 +42,7 @@ for (let index = 0; index < 18; index++) {
 }
 
 //Variables de las tarjetas al momento de hacer click
-let countCards = null;
+let countCards = 0;
 let pokemonId1 = null;
 let pokemonId2 = null;
 let cardId1 = null;
@@ -53,10 +50,108 @@ let cardId2 = null;
 
 let score = 0;
 let flipped = 0;
-
 let flips = document.getElementById("flips");
 let showScore = document.getElementById("score");
-let selCards = document.getElementsByClassName("cardsP");
+let tempo = false;
+
+const selCards = document.getElementsByClassName("cardsContainer");
+
+let timer = 90;
+let countdownTime = null;
+let initialTime= 90;
+let time = document.getElementById("time");
+
+export function countingTime(){
+  countdownTime= setInterval(()=>{
+    timer--;
+    time.innerHTML = `Tiempo: ${timer} segundos`
+  if(timer ==0){
+    clearInterval(countdownTime);
+    targetBlock()
+  }
+  },1000)
+}
+
+export function targetBlock(){
+  for(let i = 0; i <= 17; i++){
+    let oneBlockedCard= document.getElementById(`cardsContainer${i}`);
+    oneBlockedCard.className = "cardsContainer is-flipped";
+  }
+}
+
+for (const card of selCards) {
+  card.addEventListener('click',function (){
+
+    if(tempo == false){
+      countingTime();
+    tempo=true;
+    }
+
+  countCards++;
+    if (countCards ==1){ //== igualdad
+    card.classList.toggle("is-flipped");
+    pokemonId1 = card; //firstElementChild: viene toda la información almacenada
+    cardId1= card.innerText;
+    //console.log({cardId1})
+    
+  
+  } else {
+  if (countCards==2){
+    card.classList.toggle("is-flipped");
+    pokemonId2 = card;
+    cardId2= card.innerText;
+    //console.log({cardId2})
+    
+  flipped++;
+  flips.innerHTML=  `Movimientos: ${flipped} `;
+
+  if (cardId1===cardId2){
+    countCards = 0; 
+    score++;
+    showScore.innerHTML=  `Aciertos: ${score} `;
+
+    if (score==9) {
+      clearInterval(initialTime)
+      showScore.innerHTML=  `Aciertos: ${score}`;
+      time.innerHTML = `¡Muy bien! Lo hiciste en: ${initialTime-timer} segundos✨`
+      flips.innerHTML=  `Movimientos: ${flipped} `;
+
+    }
+    
+  } else {
+    
+    setTimeout(() => { pokemonId1.classList.toggle("is-flipped"), 
+        pokemonId2.classList.toggle("is-flipped");}, 1300);
+        countCards = 0;   
+
+    
+      }
+    }
+  } 
+}
+)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 
 for (const card of selCards) {
   card.addEventListener("click", function () {
@@ -106,4 +201,4 @@ function eventFire(el, etype) {
           evObj.initEvent(etype, true, false);
           el.dispatchEvent(evObj);
         }
-}
+}*/
